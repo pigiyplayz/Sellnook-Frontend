e5e383d650bff0bcc3ce476bc9958c9c57daace7
pigiyplayz <134568364+pigiyplayz@users.noreply.github.com>
2026-04-07 21:45:09 -0400

    Update waitlist.js

--- backend/routes/waitlist.js
+++ backend/routes/waitlist.js
@@ -1,1 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { addToWaitlist } = require("../controllers/waitlistController");
 
+router.post("/", addToWaitlist);
+
+module.exports = router;
